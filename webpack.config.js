0101be7a0295f9b60502ddd63d53a3e22b089c29
Alec Leong <leongalec@gmail.com>
2020-06-15 12:18:39 -0700

    updated webpack configuration to include env variables in client-side

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const dotenv = require('dotenv');
 const path = require('path');
+const webpack = require('webpack');
 
 const SRC_DIR = path.join(__dirname, 'client/src');
 const DIST_DIR = path.join(__dirname, 'client/dist');
+
+const env = dotenv.config().parsed;
+const envVariableNamesAndValues = Object.keys(env).reduce((accumulator, envVariableName) => {
+  accumulator[`process.env.${envVariableName}`] = JSON.stringify(env[envVariableName]);
+  return accumulator;
+}, {});
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -37,4 +45,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new webpack.DefinePlugin(envVariableNamesAndValues),
+  ],
 };
